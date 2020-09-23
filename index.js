4ebb611debd718c4c70b15c08f8ec252867a4a93
Shubham12051999 <shubham.navale12@gmail.com>
2020-09-23 16:22:25 +0530

    flask api

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const { spawn } = require('child_process');
+const app = express()
+var path = require('path');
+bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json());
+const {PythonShell} =require('python-shell'); 
+const port = process.env.PORT || 3000;
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/',(req,res)=>{
+    res.render('web.ejs',{'data':false})
+})
+app.get('/url',(req,res)=>{
+    res.render('web.ejs',{'data':false})
+})
+app.post('/url', (req, res) => {
+    var url=req.body.urls
+    console.log(url)
+    var dataToSend;
+    let options = { 
+        mode: 'text', 
+        pythonOptions: ['-u'], // get print results in real-time 
+        args: [url] //An argument which can be accessed in the script using sys.argv[1] 
+    }; 
+    PythonShell.run('script1.py', options, function (err, result){ 
+        if (err) return res.send(err); 
+        // console.log('result: ', result.toString()); 
+        dataToSend = result.toString().replace('{','').replace('}','').split(', ')
+        return res.render('web.ejs',{'data':dataToSend})
+  }); 
+    // spawn new child process to call the python script
+    // s='script1.py'
+    // const python = spawn('python', [s, url]);
+    // collect data from script
+    // python.stdout.on('data', function (data) {
+        // console.log('Pipe data from python script ...');
+        // dataToSend = data.toString().replace('{','').replace('}','').split(', ')
+    // });
+    // in close event we are sure that stream from child process is closed
+    // python.on('close', (code) => {
+        // console.log(`child process close all stdio with code ${code}`);
+        // send data to browser
+        // console.log(dataToSend)
+        // res.render('web.ejs',{'data':dataToSend})
+    // });
+
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ script1.py
@@ -0,0 +1,55 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+import sys
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = sys.argv[1]
+if(url[-1]!='/'):
+    url=url+'/'
+# url=url.encode()
+# print(url)
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+# Retrieve all of the anchor tags
+tags = soup('a')
+current=[]
+main=[]
+final=set()
+final.add(url)
+for tag in tags:
+    current.append(tag.get('href', None))
+for i in current:
+    if(i.find('/')==0):
+        i=url+str(i[1:])
+        main.append(i)
+    elif(i.find('http')==0):
+        main.append(i)
+    
+# print(main)
+# looping through links present in main array
+for i in main:
+    # print(i)
+    if(i in final):continue
+    final.add(i)
+    try:
+        html = urllib.request.urlopen(i, context=ctx).read()
+        soup = BeautifulSoup(html, 'html.parser')
+        tags = soup('a')
+        for tag in tags:
+            j=tag.get('href', None)
+            try:
+                if(j.find('/')==0):
+                    j=str(i)+str(j[1:])
+                    final.add(j)
+                elif(j.find('http')==0):
+                    final.add(j)
+                else:
+                    final.add(j)
+            except:continue
+    except:continue
+
+print(final)
